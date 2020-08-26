9cd4b7e5193e9f24e88e1568efd5329893ff2f2e
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-08-25 23:21:55 -0300

    Updates

---
+++ precache-manifest.54409f6312e407cbbef74991bfbec694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9252fb2db2cabbdc120da72eaa4e239c",
+    "url": "/flags-project/index.html"
+  },
+  {
+    "revision": "0058c26d511b90f7027a",
+    "url": "/flags-project/static/css/main.1ba752c1.chunk.css"
+  },
+  {
+    "revision": "481eacf1df1fcd49c5e3",
+    "url": "/flags-project/static/js/2.4ec80a44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flags-project/static/js/2.4ec80a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0058c26d511b90f7027a",
+    "url": "/flags-project/static/js/main.fc2bfa0f.chunk.js"
+  },
+  {
+    "revision": "db055fb76ec3fceb6a4a",
+    "url": "/flags-project/static/js/runtime-main.fc2e321f.js"
+  }
+]);
